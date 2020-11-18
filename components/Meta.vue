acdfae0f55c3962d64beca556bca8510188fc9ae
baround <71816368+baround@users.noreply.github.com>
2020-11-18 20:01:05 +0100

    edit target from server to static

--- components/Meta.vue
+++ components/Meta.vue
@@ -91,7 +91,7 @@ export default {
     }
 },
 mounted(){
-  console.log('this is meta')
+  // console.log('this is meta')
 }
   
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ let myRoutes = function(){
 export default {
 
   mode: 'universal',
-  target: 'server',
+  target: 'static',
   head: {
     title: '',
     meta: [
